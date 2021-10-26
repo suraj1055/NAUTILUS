f462075f99bd68614c6c290babc1d4e98ee52d92
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2021-10-26 10:19:09 +0530

    ..

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,6 +2,8 @@ import React,{Fragment} from 'react';
 import Header from './common/header-component/header';
 import Sidebar from './common/sidebar-component/sidebar';
 import RightSidebar from './common/right-sidebar';
+import Footer from './common/footer';
+import ThemeCustomizer from './common/theme-customizer'
 import { ToastContainer } from 'react-toastify';
 import Loader from './common/loader';
 
