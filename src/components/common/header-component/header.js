6d9cb2c82079321882ac31663b789aa79b143bfe
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2021-10-28 10:47:16 +0530

    Minor Changes Done

--- src/components/common/header-component/header.js
+++ src/components/common/header-component/header.js
@@ -27,7 +27,7 @@ const Header = () => {
           <div className="main-header-left d-lg-none">
             <div className="logo-wrapper">
               <Link to={`${process.env.PUBLIC_URL}/dashboard/default`}>
-                <img className="img-fluid" src={logo} alt="" />
+                
               </Link>
             </div>
           </div>
@@ -46,22 +46,7 @@ const Header = () => {
             </ul>
             <div className="d-lg-none mobile-toggle pull-right" onClick={() => setHeaderbar(!headerbar)}><MoreHorizontal/></div>
           </div>
-          <script id="result-template" type="text/x-handlebars-template">
-            <div className="ProfileCard u-cf">
-              <div className="ProfileCard-avatar">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-airplay m-0"><path d="M5 17H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-1">
-                </path>
-                  <polygon points="12 15 17 21 7 21 12 15"></polygon>
-                </svg>
-              </div>
-              {/* <div className="ProfileCard-details">
-                <div className="ProfileCard-realName"></div>
-              </div> */}
-            </div>
-          </script>
-          <script id="empty-template" type="text/x-handlebars-template">
-            <div className="EmptyMessage">{"Your search turned up 0 results. This most likely means the backend is down, yikes!"}</div>
-          </script>
+
         </div>
       </div>
     </Fragment>
