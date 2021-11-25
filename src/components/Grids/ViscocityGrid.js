af8b7c352d8c3644f5bb514b030ae865e9064365
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2021-11-25 10:54:36 +0530

    New Changes

--- src/components/Grids/ViscocityGrid.js
+++ src/components/Grids/ViscocityGrid.js
@@ -14,51 +14,53 @@ const ViscocityGrid = ({ toggle2, modal2, addRow, increaseRow, deleteRow, NewRow
         <ViscocityAddRow toggle2={toggle2} modal2={modal2} addRow={addRow} increaseRow={increaseRow} deleteRow={deleteRow} />
       </div>
       <div>
-        <Table striped bordered hover responsive variant="light">
-          <thead>
-            <tr>
-              <th> <h6> Injection Speed </h6> </th>
-              <th> <h6> Fill Time </h6> </th>
-              <th> <h6> Peak Inj Press </h6> </th>
-              <th> <h6> Viscocity </h6> </th>
-              <th> <h6> Shear Rate </h6> </th>
-              <th> <h6> AbsoluteDropViscocity </h6> </th>
-              <th> <h6> %DropViscocity </h6> </th>
-              <th> <h6> Action </h6> </th>
-            </tr>
-          </thead>
-          <tbody className="grid_style">
-            {NewRow2.map((rowId) => {
-              return (
-                <tr key={rowId}>
-                  <td> <input type='text' className="form-control" /> </td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td><input type='text' className="form-control" /> </td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow2(rowId)}></i> </td>
-                </tr>
-              )
-            })}
+        <form>
+          <Table striped bordered hover responsive variant="light">
+            <thead>
+              <tr>
+                <th> <h6> Injection Speed </h6> </th>
+                <th> <h6> Fill Time </h6> </th>
+                <th> <h6> Peak Inj Press </h6> </th>
+                <th> <h6> Viscocity </h6> </th>
+                <th> <h6> Shear Rate </h6> </th>
+                <th> <h6> AbsoluteDropViscocity </h6> </th>
+                <th> <h6> %DropViscocity </h6> </th>
+                <th> <h6> Action </h6> </th>
+              </tr>
+            </thead>
+            <tbody className="grid_style">
+              {NewRow2.map((rowId) => {
+                return (
+                  <tr key={rowId}>
+                    <td> <input type='text' className="form-control" /> </td>
+                    <td> <input type='text' className="form-control" /></td>
+                    <td><input type='text' className="form-control" /> </td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow2(rowId)}></i> </td>
+                  </tr>
+                )
+              })}
 
-            {NewRow.map((rowId) => {
-              return (
-                <tr key={rowId}>
-                  <td> <input type='text' className="form-control" /> </td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td><input type='text' className="form-control" /> </td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <input type='text' className="form-control" /></td>
-                  <td> <i className="fa fa-trash viscocity_icons"  onClick={() => deleteRow(rowId)}></i> </td>
-                </tr>
-              )
-            })}
-          </tbody>
-        </Table>
+              {NewRow.map((rowId) => {
+                return (
+                  <tr key={rowId}>
+                    <td> <input type='text' className="form-control" /> </td>
+                    <td> <input type='text' className="form-control" /></td>
+                    <td><input type='text' className="form-control" /> </td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <input type='text' className="form-control" readOnly /></td>
+                    <td> <i className="fa fa-trash viscocity_icons" onClick={() => deleteRow(rowId)}></i> </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </Table>
+        </form>
       </div>
     </div>
   )
