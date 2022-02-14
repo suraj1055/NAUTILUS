f27ee6055779374da08529e8f6de66e755c0658b
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2022-02-14 15:51:28 +0530

    ..

--- src/components/common/sidebar-component/sidebar.js
+++ src/components/common/sidebar-component/sidebar.js
@@ -29,55 +29,11 @@ const Sidebar = (props) => {
     }
 
     return (
-        <Fragment>
-            <div className="page-sidebar">
-                <div className="main-header-left d-none d-lg-block">
-                    <div>
-                            <h3 style={{ color: '#fff' }}> NAUTILUS </h3>
-                    </div>
-                </div>
-                <div className="sidebar custom-scrollbar">
-                    {/* <UserPanel /> */}
-                    <ul
-                        className="sidebar-menu"
-                        id="myDIV"
-                    >
-                        {
-                            MENUITEMS.map((menuItem, i) =>
-                                <li className={`${menuItem.active ? 'active' : ''}`} key={i}>
-                                    {(menuItem.sidebartitle) ?
-                                        <div className="sidebar-title">{menuItem.sidebartitle}</div>
-                                        : ''}
-                                    {(menuItem.type === 'sub') ?
-                                        <a className="sidebar-header" href="#javascript" onClick={() => toggletNavActive(menuItem)}>
-                                            <menuItem.icon />
-                                            <span>{props.t(menuItem.title)}</span>
-                                            <i className="fa fa-angle-right pull-right"></i>
-                                        </a>
-                                        : ''}
-                                    {(menuItem.type === 'link') ?
-                                        <Link
-                                            to={`${menuItem.path}`}
-                                            className={`sidebar-header ${menuItem.active ? 'active' : ''}`}
+        <>
 
-                                            onClick={() => toggletNavActive(menuItem)}
-                                        >
-                                            <menuItem.icon /><span>{props.t(menuItem.title)}</span>
-                                            {menuItem.children ?
-                                                <i className="fa fa-angle-right pull-right"></i> : ''}
-                                        </Link>
-                                        : ''}
-                                    {menuItem.children ?
-                                        <ul
-                                            className={`sidebar-submenu ${menuItem.active ? 'menu-open' : ''}`}
-                                            style={menuItem.active ? { opacity: 1, transition: 'opacity 500ms ease-in' } : {}}
-                                        >
-                                            {menuItem.children.map((childrenItem, index) =>
-                                                <li key={index} className={childrenItem.children ? childrenItem.active ? 'active' : '' : ''}>
-                                                    {(childrenItem.type === 'sub') ?
-                                                        <a href="#javascript" onClick={() => toggletNavActive(childrenItem)} >
-                                                            <i className="fa fa-circle"></i>{props.t(childrenItem.title)} <i className="fa fa-angle-right pull-right"></i></a>
-                                                        : ''}
+        </>
+    )
 
-                                                    {(childrenItem.type === 'link') ?
-                                                        <Link
+    }
+
+    export default Sidebar;
