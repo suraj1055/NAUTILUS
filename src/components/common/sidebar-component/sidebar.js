b29232832168b798a87009333beae8f08206a427
Suraj Vishwakarma <vishwakarmasuraj631@gmail.com>
2022-02-14 15:55:49 +0530

    Made Changes in the code flow of the six step study

--- src/components/common/sidebar-component/sidebar.js
+++ src/components/common/sidebar-component/sidebar.js
@@ -5,7 +5,6 @@ import { translate } from 'react-switch-lang';
 
 const Sidebar = (props) => {
 
-    // Click Toggle menu
     const toggletNavActive = (item) => {
         if (!item.active) {
             MENUITEMS.forEach(a => {
@@ -29,11 +28,90 @@ const Sidebar = (props) => {
     }
 
     return (
-        <>
+        <Fragment>
+            <div className="page-sidebar">
+                <div className="main-header-left d-none d-lg-block">
+                    <div>
+                            <h3 style={{ color: '#fff' }}> NAUTILUS </h3>
+                    </div>
+                </div>
+                <div className="sidebar custom-scrollbar">
+                    <ul
+                        className="sidebar-menu"
+                        id="myDIV"
+                    >
+                        {
+                            MENUITEMS.map((menuItem, i) =>
+                                <li className={`${menuItem.active ? 'active' : ''}`} key={i}>
+                                    {(menuItem.sidebartitle) ?
+                                        <div className="sidebar-title">{menuItem.sidebartitle}</div>
+                                        : ''}
+                                    {(menuItem.type === 'sub') ?
+                                        <a className="sidebar-header" href="#javascript" onClick={() => toggletNavActive(menuItem)}>
+                                            <menuItem.icon />
+                                            <span>{props.t(menuItem.title)}</span>
+                                        </a>
+                                        : ''}
+                                    {(menuItem.type === 'link') ?
+                                        <Link
+                                            to={`${menuItem.path}`}
+                                            className={`sidebar-header ${menuItem.active ? 'active' : ''}`}
 
-        </>
-    )
+                                            onClick={() => toggletNavActive(menuItem)}
+                                        >
+                                            <menuItem.icon /><span>{props.t(menuItem.title)}</span>
+                                            {menuItem.children ?
+                                                <i className="fa fa-angle-right pull-right"></i> : ''}
+                                        </Link>
+                                        : ''}
+                                    {menuItem.children ?
+                                        <ul
+                                            className={`sidebar-submenu ${menuItem.active ? 'menu-open' : ''}`}
+                                            style={menuItem.active ? { opacity: 1, transition: 'opacity 500ms ease-in' } : {}}
+                                        >
+                                            {menuItem.children.map((childrenItem, index) =>
+                                                <li key={index} className={childrenItem.children ? childrenItem.active ? 'active' : '' : ''}>
+                                                    {(childrenItem.type === 'sub') ?
+                                                        <a href="#javascript" onClick={() => toggletNavActive(childrenItem)} >
+                                                            <i className="fa fa-circle"></i>{props.t(childrenItem.title)} <i className="fa fa-angle-right pull-right"></i></a>
+                                                        : ''}
 
-    }
+                                                    {(childrenItem.type === 'link') ?
+                                                        <Link
+                                                            to={`${process.env.PUBLIC_URL}${childrenItem.path}`}
+                                                            className={childrenItem.active ? 'active' : ''}
+                                                            onClick={() => toggletNavActive(childrenItem)}
+                                                        >
+                                                            <i className="fa fa-circle"></i>{props.t(childrenItem.title)} </Link>
+                                                        : ''}
+                                                    {childrenItem.children ?
+                                                        <ul className={`sidebar-submenu ${childrenItem.active ? 'menu-open' : 'active'}`}>
+                                                            {childrenItem.children.map((childrenSubItem, key) =>
+                                                                <li className={childrenSubItem.active ? 'active' : ''} key={key}>
+                                                                    {(childrenSubItem.type === 'link') ?
+                                                                        <Link
+                                                                            to={`${process.env.PUBLIC_URL}${childrenSubItem.path}`}
+                                                                            className={childrenSubItem.active ? 'active' : ''}
+                                                                            onClick={() => toggletNavActive(childrenSubItem)}
+                                                                        >
+                                                                            <i className="fa fa-circle"></i>{props.t(childrenSubItem.title)}</Link>
+                                                                        : ''}
+                                                                </li>
+                                                            )}
+                                                        </ul>
+                                                        : ''}
+                                                </li>
+                                            )}
+                                        </ul>
+                                        : ''}
+                                </li>
+                            )
+                        }
+                    </ul>
+                </div>
+            </div>
+        </Fragment>
+    );
+};
 
-    export default Sidebar;
+export default translate(Sidebar);
